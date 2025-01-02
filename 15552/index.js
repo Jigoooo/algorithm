33f4a5e2be9bfab365dbcbad3ab00f4a2d88031b
Jigoooo <jiwook97@gmail.com>
2025-01-02 16:52:54 +0900

    15552

--- 15552/index.js
+++ 15552/index.js
@@ -2,17 +2,19 @@ const path = require('path');
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : path.join(__dirname, 'input.txt');
 
-const input = fs.readFileSync(filePath).toString().trim().split(' ').map(Number);
-const a = input[0];
-const totalLength = a / 4;
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const count = Number(input[0]);
 let result = '';
 
-for (let i = 0; i < totalLength; i++) {
-    if (i === totalLength - 1) {
-        result += 'long int';
+for (let i = 1; i <= count; i++) {
+    const [a, b] = input[i].split(' ').map(Number);
+
+    if (i === count) {
+        result += a + b;
         continue;
     }
-    result += 'long ';
+
+    result += a + b + '\n';
 }
 
 console.log(result);
