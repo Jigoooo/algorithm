6f266174746192a92ea3dd1d62fd82a2ad12609c
Jigoooo <jiwook97@gmail.com>
2025-01-08 16:06:04 +0900

    1152

---
+++ 1152/index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : path.join(__dirname, 'input.txt');
+
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+console.log(input[0].split(' ').filter((word) => word !== '').length);
