fae0d8703311679d6d30f2bf22766247d4d3302c
Jigoooo <jiwook97@gmail.com>
2025-01-12 18:36:06 +0900

    1157

--- 1157/index.js
+++ 1157/index.js
@@ -3,6 +3,19 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : path.join(__dirname, 'input.txt');
 
 const input = fs.readFileSync(filePath).toString().trim();
-const reverseInput = input.split('').reverse().join('');
+const textArray = input.split('').map(text => text.toUpperCase());
+const repeatObject = {};
 
-console.log(input === reverseInput ? 1 : 0);
+for (let i = 0; i < textArray.length; i++) {
+    if (repeatObject[textArray[i]]) {
+        repeatObject[textArray[i]] += 1;
+        continue;
+    }
+
+    repeatObject[textArray[i]] = 1;
+}
+
+const maxValue = Math.max(...Object.values(repeatObject));
+const maxKeys = Object.keys(repeatObject).filter(key => repeatObject[key] === maxValue);
+
+console.log(maxKeys.length > 1 ? '?' : maxKeys[0]);
